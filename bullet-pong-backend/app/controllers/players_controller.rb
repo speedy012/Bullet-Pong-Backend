1ed79b05f18c80961b2253da55cc7d31a3beac68
Herman Rivas <hrivas5726@aol.com>
2019-05-29 16:58:22 -0400

    controllers and models

--- bullet-pong-backend/app/controllers/players_controller.rb
+++ bullet-pong-backend/app/controllers/players_controller.rb
@@ -1,2 +1,31 @@
 class PlayersController < ApplicationController
+
+  def index
+    @players = Player.all
+  end
+
+  def new
+    @player = Player.new
+  end
+
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  def create
+    @player = Player.new(user_params)
+    if @player.valid?
+      @player.save
+      redirect_to user_path(@player)
+    else
+      flash[:user_error] = @player.errors.full_messages
+    end
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name)
+  end
+
 end
